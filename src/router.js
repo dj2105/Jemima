f5778f6268bbb78a29b8edd8d6ed7e3302609939
dj2105 <54191462+dj2105@users.noreply.github.com>
2025-09-23 03:54:54 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,13 +2,15 @@ import { Lobby } from "./views/Lobby.js";
 import { KeyRoom } from "./views/KeyRoom.js";
 import { GenerationRoom } from "./views/GenerationRoom.js";
 import { QuestionRoom } from "./views/QuestionRoom.js";
+import { MarkingRoom } from "./views/MarkingRoom.js";
 
 const routes = {
   "": Lobby,
   "#lobby": Lobby,
   "#key": KeyRoom,
   "#generation": GenerationRoom,
-  "#round1": QuestionRoom   // NEW
+  "#round1": QuestionRoom,
+  "#marking1": MarkingRoom   // NEW
 };
 
 export function router() {
